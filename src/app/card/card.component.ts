c9e369fa272e879a43973280fdc2633f0762d599
Maicon Andraski <maiconm23@gmail.com>
2020-05-11 19:23:00 -0300

    feat(liking card): action when user like a card 💖

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
+  public imageUrl = '../../assets/dog-profile.jpg';
+  public likeFlag = false;
 
   ngOnInit(): void {
+  }
+
+  public like(): void {
+    this.likeFlag = true;
   }
 
 }
