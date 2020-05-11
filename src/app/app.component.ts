28a3966d05068ebe6502b86bb3ad58b5d7db319b
Maicon Andraski <maiconm23@gmail.com>
2020-05-11 20:41:29 -0300

    fix(core): app prefix ⚒

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'cvd-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  public image = '../../assets/dog-profile.jpg';
 }
