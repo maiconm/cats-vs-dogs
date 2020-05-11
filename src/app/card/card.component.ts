bbacc1e4fdfc34c9f6715f15bc6c4d49a3bf589d
Maicon Andraski <maiconm23@gmail.com>
2020-05-11 20:33:38 -0300

    feat(animation): Card animation 🌠

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
+  animations: [
+    trigger('likeDislike', [
+      state('notShowing', style({
+        opacity: 0,
+        fontSize: 80,
+      })),
+      state('showing', style({
+        opacity: 1,
+        fontSize: 150,      })),
+      transition('notShowing => showing', [
+        animate('.25s')
+      ])
+    ])
+  ],
   selector: 'cvd-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
   public imageUrl = '../../assets/dog-profile.jpg';
   public likeFlag = false;
 
-  ngOnInit(): void {
-  }
-
   public like(): void {
     this.likeFlag = true;
+  }
+
+  public get animationTrigger(): string {
+    return this.likeFlag ? 'showing' : 'notShowing';
   }
 
 }
