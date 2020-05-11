1e1631e607cf786ec376e7c8735f9a588860c0fc
Maicon Andraski <maiconm23@gmail.com>
2020-05-11 20:58:54 -0300

    test(core): created unit test to card component 🔭

--- src/app/card/card.component.spec.ts
+++ src/app/card/card.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -8,6 +9,7 @@ describe('CardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ NoopAnimationsModule ],
       declarations: [ CardComponent ]
     })
     .compileComponents();
@@ -21,5 +23,27 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('image url should be undefined when created', () => {
+    expect(component.imageUrl).toBeUndefined();
+  });
+
+  it('like flag should be false when created', () => {
+    expect(component.likeFlag).toBeFalse();
+  });
+
+  it('like flag should be true when call like method', () => {
+    component.like();
+    expect(component.likeFlag).toBeTrue();
+  });
+
+  it('animationTrigger() should return notShowing after created', () => {
+    expect(component.animationTrigger).toBe('notShowing');
+  });
+
+  it('animationTrigger() should return showing after call like()', () => {
+    component.like();
+    expect(component.animationTrigger).toBe('showing');
   });
 });
