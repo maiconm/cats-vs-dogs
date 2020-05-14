33cd169964e18d8f4328ec8ef39438bcb6009433
Maicon Andraski <maiconm23@gmail.com>
2020-05-13 21:11:25 -0300

    feat(core): dynamic images 🐶

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CatAndDogsHttpService } from './services/cat-and-dogs-http.service';
+import { Cat } from './interfaces/cat';
+import { Dog } from './interfaces/dog';
 
 @Component({
   selector: 'cvd-root',
@@ -6,11 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public image = '../../assets/dog-profile.jpg';
-  public image2 = '../../assets/cat-profile.jpg';
-  public canLike = true;
+  public cat: Cat;
+  public dog: Dog;
+  public canLike: boolean;
+
+  constructor(private cdHttp: CatAndDogsHttpService) {
+    this.getCatAndDog();
+  }
 
   public likeHandler(): void {
     this.canLike = false;
+    this.getCatAndDog();
+  }
+
+  private getCatAndDog(): void {
+    this.cdHttp.catAndDogRequest().subscribe(([cat, dog]) => {
+      this.cat = cat;
+      this.dog = dog;
+      this.canLike = true;
+    });
   }
 }
--- src/app/services/cat-and-dogs-http.service.ts
+++ src/app/services/cat-and-dogs-http.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
+import { map, retry, retryWhen, tap } from 'rxjs/operators';
+import { Dog } from '../interfaces/dog';
+import { Cat } from '../interfaces/cat';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +12,31 @@ export class CatAndDogsHttpService {
 
   constructor(private http: HttpClient) { }
 
+  private getDog(): Observable<Dog> {
+    return this.http.get<Dog>('https://random.dog/woof.json').pipe(
+      map(this.handleDogResponse),
+      retryWhen(this.handleError)
+    );
+  }
+
+  private getCat(): Observable<Cat> {
+    return this.http.get<Cat>('https://aws.random.cat/meow');
+  }
+
+  public catAndDogRequest() {
+    return forkJoin([this.getCat(), this.getDog()]);
+  }
+
+  private handleDogResponse(dog: Dog): Dog {
+    if (!dog.url.endsWith('.mp4')) {
+      return dog;
+    }
+    throw new Error('.mp4 file not supported');
+  }
+
+  private handleError(error: Observable<any>): Observable<void> {
+    return error.pipe(
+      tap(() => console.warn('💩 oh oh', error))
+    );
+  }
 }
